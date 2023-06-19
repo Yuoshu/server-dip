feaedd22d3714f63e03024a88d621639d3e7bd3a
Yuoshu <91850792+Yuoshu@users.noreply.github.com>
2023-06-19 15:17:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-import express from "express";
-import colors from "colors";
-import dotenv from "dotenv";
-import morgan from "morgan";
+const express = require("express")
+const dotenv = require("dotenv")
+const morgan = require("morgan")
+const cors = require("cors")
+
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-import cors from "cors";
+
 
 //configure env
 dotenv.config();
@@ -38,7 +39,6 @@ const PORT = process.env.PORT || 8080;
 //run listen
 app.listen(PORT, () => {
   console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
-      .white
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`
   );
 });
