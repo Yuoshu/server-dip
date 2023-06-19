1a20d2e6ff5e18c75bad47035b8e10798b0f1aa3
Yuoshu <91850792+Yuoshu@users.noreply.github.com>
2023-06-19 15:32:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express")
-const dotenv = require("dotenv")
-const morgan = require("morgan")
-const cors = require("cors")
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
 
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
